0cfc7f6bed92e200bb2393d434c9e6347316bf60
serfertor <serfertor@mail.ru>
2025-02-23 22:39:28 +0300

    main change to rknn2

--- testnpu.py
+++ testnpu.py
@@ -2,8 +2,13 @@ from ultralytics import YOLO
 
 # Load the exported RKNN model
 rknn_model = YOLO("weights/bestn_rknn_model/bestn_rknn_model")
+model = YOLO("weights/best (1).pt")
 
 # Run inference
 results = rknn_model("test.jpg")
 
+results1 = model("test.jpg")
+
 print(results[0])
+
+print(results1[0])
