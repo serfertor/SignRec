7307279fa6f09dd063a6d10b94c82db06ffba24f
serfertor <serfertor@mail.ru>
2025-02-26 00:06:32 +0300

    отладка18+видео

--- testnpu.py
+++ testnpu.py
@@ -2,13 +2,6 @@ from ultralytics import YOLO
 
 # Load the exported RKNN model
 rknn_model = YOLO("weights/bestn_rknn_model/bestn_rknn_model")
-model = YOLO("weights/best (1).pt")
 
 # Run inference
-results = rknn_model("test.jpg")
-
-results1 = model("test.jpg")
-
-print(results[0])
-
-print(results1[0])
+results = rknn_model.predict("video.mp4", show=True)
